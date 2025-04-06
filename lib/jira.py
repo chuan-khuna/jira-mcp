5dc2a21413ba109eb936197c3ef681fc056ef094
ALTR <chuan.khuna@gmail.com>
2025-04-06 21:27:30 +0700

    add jira query client

---
+++ lib/jira.py
@@ -0,0 +1,86 @@
+import requests
+from requests.auth import HTTPBasicAuth
+import json
+import pandas as pd
+from tqdm import tqdm
+
+
+class JiraClient:
+    def __init__(self, url: str, email: str, token: str):
+        self.email = email
+        self.token = token
+        self.url = url
+        self.http_auth = HTTPBasicAuth(self.email, self.token)
+
+        self.filtered_columns = [
+            "project.name",
+            "key",
+            "status.name",
+            "issuetype.name",
+            "assignee.displayName",
+            "priority.name",
+            "customfield_10016",
+            "created",
+            "updated",
+            "duedate",
+            "timeestimate",
+            "aggregatetimeestimate",
+            "timeoriginalestimate",
+            "aggregatetimeoriginalestimate",
+            "timespent",
+            "aggregatetimespent",
+            "summary",
+        ]
+
+    def _build_jql_query_string(self, days: int, task_types: list[str]) -> str:
+        task_tuple = ', '.join(task_types)
+        task_condition = f'AND issuetype in ({task_tuple})' if task_types else ''
+
+        jql_string = f'created >= -{days}d {task_condition} order by created DESC'
+        return jql_string
+
+    def _paginated_query(
+        self, days: int, task_types: list[str], skip: int, limit: int
+    ) -> None | pd.DataFrame:
+        headers = {"Accept": "application/json"}
+        query = {
+            'jql': self._build_jql_query_string(days, task_types),
+            'startAt': skip,
+            'maxResults': limit,
+        }
+
+        response = requests.request(
+            "GET", self.url, headers=headers, params=query, auth=self.http_auth
+        )
+
+        data = json.loads(response.text)
+        issues = data.get('issues', [])
+
+        if len(issues) == 0:
+            return None
+
+        df = pd.DataFrame(issues)
+        df = df.join(pd.json_normalize(df['fields']))
+
+        try:
+            return df[self.filtered_columns]
+        except KeyError as e:
+            print(f"Error while filtering columns from Jira response: {e}")
+            return None
+
+    def query_tasks(
+        self, num_pages: int, page_size: int, days: int, task_types: list[str]
+    ) -> pd.DataFrame:
+        dfs = []
+        for page in tqdm(range(num_pages), desc="Fetching pages"):
+            skip = page * page_size
+            df = self._paginated_query(days, task_types, skip=skip, limit=page_size)
+
+            if df is not None:
+                dfs.append(df)
+            else:
+                print(f"No more issues found or an error occurs after page {page}.")
+                break
+
+        df = pd.concat(dfs, ignore_index=True)
+        return df
